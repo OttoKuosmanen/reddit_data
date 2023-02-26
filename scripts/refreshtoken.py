6ece8268e7c19e0c8344eda1da7c1639d9e3655b
Keijumies <125501964+Keijumies@users.noreply.github.com>
2023-02-26 17:16:44 +0100

    Update refreshtoken.py
    Gets all the posts

--- scripts/refreshtoken.py
+++ scripts/refreshtoken.py
@@ -2,6 +2,7 @@
 
 import requests
 import requests.auth
+import pandas as pd
 client_auth = requests.auth.HTTPBasicAuth('52b_-ogrKMs1rpbT0d-TJQ', 'IQF_XzdHCsPHXvWcNqGbD1YCmZurGA')
 post_data = {"grant_type": "password", "username": "Otto_kuosmanen", "password": "reddIt3?"}
 headers = {"User-Agent": "advicedata/0.1 by Otto_kuosmanen"}
@@ -13,5 +14,11 @@ headers['Authorization'] = f'bearer {TOKEN}'
 #print(requests.get('https://oauth.reddit.com/api/v1/me', headers=headers).json())
 
 res = requests.get('https://oauth.reddit.com/r/advice/hot', headers = headers)
-print(res.json())
+ress = res.json()
 
+#keys: data:children:0-24:data:selftext
+
+for x in range(25):
+    print(ress["data"]["children"][x]["data"]["selftext"]) # here dictionary uses keys and the list inside uses index
+
+#dictionary
